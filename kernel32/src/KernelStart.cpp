feca823f6388e04435e9dacd02d71c3eeaae5974
0x00000FF <doctor@kangnang.net>
2018-08-30 20:18:37 +0900

    parepare page entry data structure

--- kernel32/src/KernelStart.cpp
+++ kernel32/src/KernelStart.cpp
@@ -4,11 +4,12 @@
 #include <Constants.hpp>
 
 const void* VGABUFFER    =  (const void*)  0xB8000;
-
-const DWORD KernelPrints          (const int, const int, const char*, const BYTE = 0x07);
-      void  KernelMessagePrint    (const int, const int, const char*, const bool);
-      bool  KernelCheckMemorySize ();
-      bool  KernelInit64Area      ();
+      
+DWORD KernelPrints          (const int, const int, const char*, const BYTE = 0x07);
+void  KernelMessagePrint    (const int, const int, const char*, const bool);
+bool  KernelCheckMemorySize ();
+bool  KernelInit64Area      ();
+void  KernelStop            ();
 
 void KernelStart() {
     KernelPrints(0, 0, "PHILOSOPHER'S STONE :: DEV ALPHA, 0.1", CON_MAGENTA);
@@ -16,20 +17,25 @@ void KernelStart() {
 
     KernelPrints(0, 2, "32Bit Protected Mode SUCCESS!!", CON_LIGHT_GREEN);
 
-    KernelMessagePrint(0, 3, "Minimum Memory Requirement Check....", KernelCheckMemorySize());
+    bool isMemoryEnough = KernelCheckMemorySize();
+    KernelMessagePrint(0, 3, "Minimum Memory Requirement Check....", isMemoryEnough);
+    if (!isMemoryEnough) {
+        KernelPrints(0, 4, "Memory size check failed, PS64 requires 64MB or larger system memory.", CON_LIGHT_RED);
+        KernelStop();
+    }
+
     KernelMessagePrint(0, 4, "IA-32e Kernel Area Initialization...", KernelInit64Area());
     KernelMessagePrint(0, 5, "IA-32e Page Table Initialization....", false);
 
     KernelPrints(0, 8, "SYSTEM FAILURE", _CONSOLE_ATTRIBUTE(CON_WHITE, CON_LIGHT_RED));
-
-    while (true) ;
+    KernelStop();
 }
 
-const DWORD KernelPrints(
-        const int xPos, 
-        const int yPos, 
-        const char* str,
-        const BYTE color 
+DWORD KernelPrints(
+        const int    xPos, 
+        const int    yPos, 
+        const char*  str,
+        const BYTE   color 
      ) {
     DWORD      i      = 0;
     CHARACTER* screen = (CHARACTER*) VGABUFFER;
@@ -44,11 +50,11 @@ const DWORD KernelPrints(
 }
 
 void KernelMessagePrint (
-          const int xPos,
-          const int yPos,
-          const char* msg,
-          const bool status
-        ) {
+        const int    xPos,
+        const int    yPos,
+        const char*  msg,
+        const bool   status
+    ) {
     auto printedCount = KernelPrints(xPos, yPos, msg);
     KernelPrints(xPos + printedCount, yPos, "[");
     
@@ -93,3 +99,6 @@ bool KernelInit64Area() {
     return true;
 }
 
+void KernelStop() {
+    while (true) ;
+}
--- kernel32/src/include/Types.hpp
+++ kernel32/src/include/Types.hpp
@@ -20,4 +20,19 @@ typedef struct kCharacterStruct {
 
 #pragma pack( pop )
 
+typedef struct pageTableEntryStruct {
+    BYTE    P           : 1;
+    BYTE    RW          : 1;
+    BYTE    US          : 1;
+    BYTE    PWT         : 1;
+    BYTE    PCD         : 1;
+    BYTE    A           : 1;
+    BYTE    Reserved_1  : 3;
+    BYTE    Avail       : 3;
+    DWORD   BaseAddress : 28;
+    WORD    Reserved_2  : 12 = 0;
+    WORD    Avail2      : 11;
+    BYTE    EXB         : 1;
+} PML4TENTRY, PDPTENTRY, PDENTRY, PTENTRY;
+
 #endif
