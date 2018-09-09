e401a1913e1bb21d9972240df13956fc8b1cb7bc
0x00000FF <doctor@kangnang.net>
2018-09-09 10:43:07 +0900

    add segment descriptor for IA-32e

--- kernel32/src/KernelProtectEntry.s
+++ kernel32/src/KernelProtectEntry.s
@@ -82,7 +82,27 @@ GDT:
         db 0x00
         db 0x00
         db 0x00
-        
+
+    IA_32eCODEDESCRIPTOR:
+        dw 0xFFFF  
+        dw 0x0000 
+        db 0x00  
+
+        db 0x9A    ; P=1, DPL=0, Code Segment (Execute/Read)
+        db 0xAF    ; G=1, D=0, L=1, Limit[19:16]
+
+        db 0x00
+
+    IA_32eDATADESCRIPTOR:
+        dw 0xFFFF
+        dw 0x0000
+        db 0x00
+
+        db 0x92    ; P=1, DPL=0, Data Segment (Read/Write)
+        db 0xAF    ; G=1, D=0, L=1, Limit[19:16]
+
+        db 0x00
+
     CODESEGMENT:
         dw  0xFFFF ; Segment Size Limit
         dw  0x0000 ; Base
